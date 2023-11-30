26fc5cad8ba094906406a783982b2b344ee022e9
jap <japdavev5@gmail.com>
2023-11-30 19:20:26 +0530

    updated

---
+++ routes/annualScheme.routes.js
@@ -0,0 +1,16 @@
+const express=require("express")
+const annualRouter=express.Router()
+const annualSchemeController=require("../controller/annualSchemeController")
+annualRouter.post("/annual/manager/add",annualSchemeController.addManager)
+annualRouter.get("/annual/manager",annualSchemeController.getManager)
+annualRouter.post("/annual/login",annualSchemeController.login)
+annualRouter.delete("/annual/manager/delete/:id",annualSchemeController.deleteManager)
+annualRouter.get("/annual/manager/search/:name",annualSchemeController.searchManager)
+annualRouter.post("/annual/add",annualSchemeController.generateToken)
+annualRouter.get("/annual",annualSchemeController.getToken)
+annualRouter.get("/annual/:token",annualSchemeController.viewByToken)
+annualRouter.post("/annual/edit/:token",annualSchemeController.editByToken)
+annualRouter.get("/annual/view/:id",annualSchemeController.viewToken)
+module.exports={
+    annualRouter
+}
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const multer = require("multer");
 const sharp = require("sharp");
 const app = express();
 const fs = require("fs");
+const path=require("path")
 const { categoryRouter } = require("./routes/category.routes");
 const { productRouter } = require("./routes/product.routes");
 const { cardRouter } = require("./routes/card.routes");
@@ -15,6 +16,9 @@ const { aboutRouter } = require("./routes/about.routes");
 const { newsRouter } = require("./routes/news.routes");
 const { broucherRouter } = require("./routes/broucher.routes");
 const { inquiryRouter } = require("./routes/inquiry.routes");
+const { annualRouter } = require("./routes/annualScheme.routes");
+const { outletRouter } = require("./routes/outlet.routes");
+const { navbarRouter } = require("./routes/navbar.routes");
 app.use(express.json());
 require("dotenv").config();
 app.use(cors({ origin: true }));
@@ -118,6 +122,9 @@ app.use("/", aboutRouter);
 app.use("/", newsRouter);
 app.use("/",broucherRouter)
 app.use("/",inquiryRouter)
+app.use("/",annualRouter)
+app.use("/",outletRouter)
+app.use("/",navbarRouter)
 
 app.post("/broucher/pdf",upload5.single("broucher"),async(req,res)=>{
   console.log(req.file);
